be29b6e449c155bb7c31e1850753fda4253dbd99
austin-sanga <austinsanga2000@gmail.com>
2022-06-25 12:19:28 +0300

    lesson 8

--- resources/views/lesson8.blade.php
+++ resources/views/lesson8.blade.php
@@ -8,11 +8,28 @@
 </style>
 
 {{-- navbar --}}
+<section id="intro">
+
+</section>
 
 
 {{-- main image $intro text --}}
 <section id="intro">
-
+    <div class="container-lg">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-md-5 text-center text-md-start">
+                <h1>
+                    <div class="display-2">Black belt</div>
+                    <div class="dispaly-5">Your Coding Skills</div>
+                </h1>
+                <p class="lead my-4 text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <a href="#pricing" class="btn btn-secondary btn-lg">Buy now</a>
+            </div>
+            <div class="col-md-5 text-center d-none d-md-block">
+               <img class="img-fluid" src="/media/ebook-cover.png" alt="ebook cover">
+            </div>
+        </div>
+    </div>
 </section>
 
 
@@ -29,4 +46,3 @@
 @stop
 
 
-{{-- comments push --}}
